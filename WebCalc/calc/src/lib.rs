0ea2f083d3a38ab6bfdf6a1c8367990d411983ba
vafadehghan <vafa@dehghan.ca>
2020-08-17 15:56:34 -0700

    removed unnecesary files

--- WebCalc/calc/src/lib.rs
+++ WebCalc/calc/src/lib.rs
@@ -1,65 +1,61 @@
-pub extern fn main() {
+pub extern "C" fn main() {
     println!("Hello, world!");
 }
 
 #[no_mangle]
-fn fib(n: usize) ->u32 
-{  
-    if n == 0{
-        return 0
+fn fib(n: usize) -> u32 {
+    if n == 0 {
+        return 0;
     }
-    if n == 1{
-        return 1
+    if n == 1 {
+        return 1;
     }
-    let mut fib = vec![0; n+2];
+    let mut fib = vec![0; n + 2];
     fib[0] = 0;
     fib[1] = 1;
-    for i in 2..n+1
-    { 
-       fib[i] = fib[i - 1] + fib[i - 2]; 
-    } 
-    return fib[n];  
+    for i in 2..n + 1 {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
 }
 
 #[no_mangle]
-fn fibrec(n: usize) ->usize 
-{  
+fn fibrec(n: usize) -> usize {
     if n <= 1 {
         return n;
     }
-    return fibrec(n-1) + fibrec(n-2);
+    return fibrec(n - 1) + fibrec(n - 2);
 }
 
 #[no_mangle]
-fn catalan(n: usize) -> u32{
-    if n == 0{
-        return 1
+fn catalan(n: usize) -> u32 {
+    if n == 0 {
+        return 1;
     }
-    if n == 1{
-        return 1
+    if n == 1 {
+        return 1;
     }
-    let mut cat = vec![0; n+1];
+    let mut cat = vec![0; n + 1];
     cat[0] = 1;
     cat[1] = 1;
-    for i in 2..n+1{
+    for i in 2..n + 1 {
         cat[i] = 0;
-        for j in 0..i{
-            cat[i] += cat[j] * cat[i-j-1];
+        for j in 0..i {
+            cat[i] += cat[j] * cat[i - j - 1];
         }
     }
-    return cat[n]
-} 
+    return cat[n];
+}
 
 #[no_mangle]
-fn catalanrec(n: u32) ->u32 
-{  
-    if n <= 1{
-        return 1; 
-    } 
-    let mut result = 0; 
-    for i in 0..n{
-        result += catalanrec(i)*catalanrec(n-i-1); 
+fn catalanrec(n: u32) -> u32 {
+    if n <= 1 {
+        return 1;
     }
-        
-    return result; 
-} 
+    let mut result = 0;
+    for i in 0..n {
+        result += catalanrec(i) * catalanrec(n - i - 1);
+    }
+
+    return result;
+}
--- cookbooks/polyglot/recipes/default.rb
+++ cookbooks/polyglot/recipes/default.rb
@@ -1,5 +1,4 @@
 ubuntu_mirror = 'http://ubuntu.mirror.rafal.ca/ubuntu/'
-# ubuntu_mirror = 'http://mirror.csclub.uwaterloo.ca/ubuntu/'
 ubuntu_release = 'bionic'
 ubuntu_version = '18.04'
 username = 'vagrant'
@@ -39,30 +38,7 @@ package ['build-essential', 'cmake']
 # Other core language tools you might want
 
 package ['python3', 'python3-pip', 'python3-dev']  # Python
-#package ['ghc', 'libghc-random-dev', 'cabal-install']  # Haskell
-#package 'golang-go'  # Go
-#package 'erlang'  # Erlang
-#package 'ocaml-nox'  # OCaml
 package ['rustc', 'cargo']  # Rust
-#package 'scala'  # Scala 2.11
-#package ['ruby', 'ruby-dev']  # Ruby
-#package ['openjdk-11-jdk', 'maven']  # Java
-#package ['php-cli', 'php-pear']  # PHP
-#package 'clang' # Clang C/C++ compiler
-
-
-
-# SBT
-#execute 'sbt apt key' do
-#  command 'curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add'
-#  not_if 'apt-key list | grep "2EE0 EA64"'
-#end
-#file '/etc/apt/sources.list.d/sbt.list' do
-#  content 'deb https://dl.bintray.com/sbt/debian /'
-#  notifies :run, 'execute[apt-get update]', :immediately
-#end
-#package 'sbt'
-
 
 
 # NodeJS (more modern than Ubuntu nodejs package) and NPM
@@ -78,100 +54,7 @@ end
 package ['nodejs']
 
 
-# SWIG
-
-#package 'swig'
-
-
-# RabbitMQ-related things
-
-#package ['rabbitmq-server']
-
-# Python pika library
-#execute 'pip3 install pika==1.1.0' do
-#  creates "#{python3_packages}/pika/__init__.py"
-#end
-# Ruby bunny library
-#execute 'gem install bunny -v 2.15.0' do
-#  creates "#{ruby_gems}/bunny-2.15.0/Gemfile"
-#end
-# Go amqp library
-#execute 'go get github.com/streadway/amqp github.com/google/uuid' do
-#  cwd project_home 
-#  user username
-#  environment 'HOME' => user_home
-#  creates user_home + '/go/src/github.com/streadway/amqp/README.md'
-#end
-# Java amqp library
-#package 'librabbitmq-client-java'
-
-
-# ZeroMQ-related things
-
-# C/C++ library and dev library
-#package ['libzmq5', 'libzmq5-dev']
-# Python pyzmq library
-#execute 'pip3 install pyzmq==19.0.1' do
-#  creates "#{python3_packages}/zmq/__init__.py"
-#end
-# Ruby ezmq library
-#execute 'gem install ezmq -v 0.4.12' do
-#  creates "#{ruby_gems}/ezmq-0.4.12/Gemfile"
-#end
-# Node zmq library
-#execute 'npm install zeromq@6.0.0-beta.6' do
-#  cwd project_home
-#  user username
-#  environment 'HOME' => user_home
-#  creates project_home + '/node_modules/zeromq/package.json'
-#end
-# Go zmq4 library
-#execute 'go get github.com/pebbe/zmq4' do
-#  cwd project_home 
-#  user username
-#  environment 'HOME' => user_home
-#  creates user_home + '/go/pkg/linux_amd64/github.com/pebbe/zmq4.a'
-#end
-
-
-# GraalVM
-
-#graalvm_version = '20.1.0'
-#graalvm_directory = "graalvm-ce-java11-#{graalvm_version}"
-#remote_file '/opt/installers/graalvm.tar.gz' do
-#  source "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-#{graalvm_version}/graalvm-ce-java11-linux-amd64-#{graalvm_version}.tar.gz"
-#end
-#execute 'tar zxf /opt/installers/graalvm.tar.gz' do
-#  cwd '/opt'
-#  creates "/opt/#{graalvm_directory}/release"
-#end
-#link '/opt/graalvm' do
-#  to "/opt/#{graalvm_directory}"
-#  link_type :symbolic
-#end
-#file '/etc/profile.d/graalvm.sh' do
-#  content 'PATH=${PATH}:/opt/graalvm/bin'
-#end
-# GraalVM's Python
-#execute '/opt/graalvm/bin/gu install python' do
-#  creates "/opt/#{graalvm_directory}/bin/graalpython"
-#end
-# GraalVM's Ruby
-#execute '/opt/graalvm/bin/gu install ruby' do
-#  creates "/opt/#{graalvm_directory}/bin/ruby"
-#end
-# GraalVM's LLVM (C, C++) tools
-#execute '/opt/graalvm/bin/gu install llvm-toolchain' do
-#  creates "/opt/#{graalvm_directory}/bin/lli"
-#end
-
-# execute 'npm install open' do
-#  cwd project_home
-#  user username
-#  environment 'HOME' => user_home
-#  creates project_home + '/node_modules/open/package.json'
-# end
-
+#
 execute 'npm install --no-bin-links' do
   cwd project_home + '/server'
   user username
